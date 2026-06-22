4df6470735b26da9c0ea0a8ac30ec54be1cfe697
krishu745956-hash <krishu745956@gmail.com>
2026-06-22 16:52:24 +0530

    debug: add error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,24 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+class ErrorBoundary extends React.Component {
+  state = { error: null }
+  componentDidCatch(error) { this.setState({ error }) }
+  render() {
+    if (this.state.error) return (
+      <div style={{color:'red',padding:20,fontFamily:'monospace',whiteSpace:'pre-wrap'}}>
+        {String(this.state.error)}
+      </div>
+    )
+    return this.props.children
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ErrorBoundary>
     <BrowserRouter basename="/Periodic-Table">
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </ErrorBoundary>
 )
+
